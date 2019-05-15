ab5cb09342e2551f2a94df5ba83e0b5bcdf50182
Dmytriy Myk <porosya@gmail.com>
2019-05-15 19:41:30 +0300

    solved betweeb markers

--- Elementary/between_markers_simplified.py
+++ Elementary/between_markers_simplified.py
@@ -1,26 +1,23 @@
-#!/home/porosya/.local/share/virtualenvs/checkio-VEsvC6M1/bin/checkio --domain=py run between-markers-simplified
+#!/home/porosya/.local/share/virtualenvs/checkio-VEsvC6M1/bin/checkio --domain=py check between-markers-simplified
 
 # https://py.checkio.org/mission/between-markers-simplified/
 
 # You are given a string and two markers (the initial one and final). You have to find a substring enclosed between these two markers. But there are a few important conditions.
-# 
+#
 # This is a simplified version of theBetween Markersmission.
-# 
+#
 # The initial and final markers are always different.The initial and final markers are always 1 char size.The initial and final markers always exist in a string and go one after another.Input:Three arguments. All of them are strings. The second and third arguments are the initial and final markers.
-# 
+#
 # Output:A string.
-# 
+#
 # Precondition:There can't be more than one final and one initial markers.
-# 
-# 
+#
+#
 # END_DESC
 
+
 def between_markers(text: str, begin: str, end: str) -> str:
-    """
-        returns substring between two given markers
-    """
-    # your code here
-    return ''
+    return(text[text.find(begin)+1:text.rfind(end)])
 
 
 if __name__ == '__main__':
